aec16b924c88489df11d46b712334c311844dcb2
Oliver Stigley <oliver@boost.co.nz>
2017-06-19 19:16:30 +1200

    WHAT: convert to mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616014935) do
+ActiveRecord::Schema.define(version: 20170619062144) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "games_teams", id: false, force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "team_id", null: false
+    t.index ["game_id", "team_id"], name: "index_games_teams_on_game_id_and_team_id"
+    t.index ["team_id", "game_id"], name: "index_games_teams_on_team_id_and_game_id"
   end
 
   create_table "players", force: :cascade do |t|
