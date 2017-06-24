441b5895b42b35524a5b2c2c9e2a18faad6d7eb0
Oliver Stigley <oliver@boost.co.nz>
2017-06-24 15:06:05 +1200

    WHAT: player can update the winner of a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,7 @@ class GamesController < ApplicationController
   before_action :require_login, only: [:new, :create, :edit, :update]
   before_action :find_game, only: [:show, :edit, :update]
   before_action :player_in_game, only: [:edit, :update]
+  before_action :select_team, only: [:update]
 
   def new
     @game = Game.new
@@ -27,21 +28,31 @@ class GamesController < ApplicationController
     @games = Game.all
   end
 
-  def edit
+  def update
+    if @game.create_winner(team: @team)
+      redirect_to games_path id: @game.id
+    else
+      flash[:notice] = @game.errors
+    end
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:id, :team_ids => [])
+    params.require(:game).permit(:id, :team_id, :winner, :team_ids => [])
   end
 
   def find_game
-    @game = Game.find_by_id(params[:id])
+    @game ||= Game.find_by_id(params[:id])
   end
 
   def player_in_game
     redirect_to games_index_path unless @game.players.include? current_player
   end
+
+  def select_team
+    @team = @game.teams.find { |team| team.id == game_params[:winner].to_i }
+  end
+
 
 end
