5b48fc6434c91f3860f3b740902868655713ca31
Oliver Stigley <oliver@boost.co.nz>
2017-06-25 15:22:22 +1200

    WHAT: tests for updating a game with a winning team

--- spec/features/games/upadte_game_spec.rb
+++ spec/features/games/upadte_game_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Updating a game', type: :feature do
   let(:edit_game_page) { EditGame.new }
   let(:sign_in_page) { SignInPage.new }
-  let(:signed_in_player) { create(:player) }
 
   context 'for non-logged in players' do
     scenario 'redirects to sign in page' do
@@ -13,10 +12,12 @@ RSpec.feature 'Updating a game', type: :feature do
   end
 
   context 'for logged in players' do
-
+    before do
+      @game = create(:game)
+    end
     context 'for players that did not play' do
       scenario 'redirects to the games index page' do
-        @game = create(:game)
+        signed_in_player = create(:player)
         set_omniauth(signed_in_player)
 
         sign_in_page.load
@@ -28,14 +29,34 @@ RSpec.feature 'Updating a game', type: :feature do
     end
 
     context 'for players that did play in the game' do
+      before do
+        # Make the signed in player part of the team
+        signed_in_player = @game.players.first
+        set_omniauth(signed_in_player)
+
+        sign_in_page.load
+        sign_in_page.github.click
+        edit_game_page.load(id: @game.id)
+      end
+
       scenario 'renders the update game form' do
+        expect(edit_game_page).to have_edit_game_form
+      end
+
+      scenario 'renders the team names' do
+        expect(edit_game_page).to have_content(@game.teams.first.team_name)
+        expect(edit_game_page).to have_content(@game.teams.second.team_name)
       end
 
       scenario 'lets the player set a winner' do
+        winner_button = edit_game_page.winner_radio_buttons.last
+        winner_button.click
+        edit_game_page.submit_button.click
+
+        expect(page.current_path).to eq games_path
       end
 
-      scenario 'lets the player confirm the winner' do
-      end
+      scenario 'lets the player confirm the winner'
     end
   end
  end
--- spec/page_objects/edit_game_page.rb
+++ spec/page_objects/edit_game_page.rb
@@ -1,4 +1,7 @@
 class EditGame < SitePrism::Page
   set_url '/games/edit{?id*}'
 
+  element :edit_game_form, '.edit-game-form'
+  elements :winner_radio_buttons, '.edit-game-form__radio-buttons'
+  element :submit_button, '#submit'
 end
