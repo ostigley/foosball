3c64ad39a5bb998aa445cce5be4501b4f7aebb13
Oliver Stigley <oliver@boost.co.nz>
2017-06-30 15:01:46 +1200

    add total games played to leaderboard

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,6 +2,7 @@ class LeaderboardsController < ApplicationController
   include LeaderboardStats
 
   def index
+    @game_count = Game.all.count
     @teams = LeaderboardStats::Leaderboard.new(Team.all).team_leaderboard
     @players = LeaderboardStats::Leaderboard.new(nil, Player.all).player_leaderboard
   end
