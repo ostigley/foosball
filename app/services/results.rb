1cd57d91477de7702193aece849dc7ecf6724cfb
Oliver Stigley <oliver@boost.co.nz>
2017-07-03 19:15:38 +1200

    change arguments for results service module

--- app/services/results.rb
+++ app/services/results.rb
@@ -1,15 +1,19 @@
 module Results
   class Result
-    def initialize(game, winner)
+    def initialize(game)
       @game = game
-      @winner = winner
-      @loser = @game.teams.find {|team| team != @winner }
     end
 
-    def set_winner
-      @game.create_winner(team: @winner)
-      @game.create_loser(team: @loser)
+    def set_winner(winner)
+      loser = @game.teams.find {|team| team != winner }
+      @game.create_winner(team: winner)
+      @game.create_loser(team: loser)
       @game.save!
+    end
+
+    def confirm_winner(winner, confirmation)
+      winner.confirmed = confirmation
+      winner.save!
     end
   end
 end
