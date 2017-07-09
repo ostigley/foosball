11a23fb52f277e38aeefaab9b4250f366f868636
Oliver Stigley <oliver@boost.co.nz>
2017-07-10 11:50:02 +1200

    fix mail url host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "foosball_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'foosball-boost.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
