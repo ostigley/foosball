9e058d44253bd3b489387c44b3ad4164ce4ecf78
Oliver Stigley <oliver@boost.co.nz>
2017-07-14 13:13:59 +1200

    new game spec

--- spec/features/games/new_game_spec.rb
+++ spec/features/games/new_game_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'New game page', type: :feature do
+RSpec.feature 'New game page', type: :feature, js: true do
   let(:new_game_page) { NewGame.new }
   let(:sign_in_page) { SignInPage.new }
 
@@ -15,8 +15,9 @@ RSpec.feature 'New game page', type: :feature do
 
   context 'for logged in users' do
     before do
-      @teams = create_list(:team, 3, :full_team)
+      @teams = create_list(:team, 5, :full_team)
       set_omniauth(signed_in_player)
+      Team.last.players = [Player.first, signed_in_player]
       sign_in_page.load
       sign_in_page.github.click
       new_game_page.load
@@ -27,19 +28,44 @@ RSpec.feature 'New game page', type: :feature do
     end
 
     context 'the new team form' do
-      scenario 'renders the list of teams' do
-        expect(page).to have_content(@teams.first.team_name)
-        expect(page).to have_content(@teams.second.team_name)
+      scenario 'renders the list of my teams' do
+        expect(new_game_page).to have_my_teams count: 2
+      end
+
+      scenario 'renders the list of oposition teams' do
+        expect(new_game_page.other_teams.count).to eq 5
       end
     end
 
-    context 'createing a new team' do
-      scenario 'renders the root path' do
-        new_game_page.teams[1].click
-        new_game_page.teams[2].click
-        new_game_page.submit.click
-        expect(page).to have_content 'This game...'
-        expect(page.current_path).to match '/games?'
+    context 'creating a new game' do
+      context 'selecting a team mate' do
+        scenario 'removes all team mates other teams from the list' do
+          expect(new_game_page.other_teams.count).to eq 5
+          new_game_page.my_team_select.select(Player.first.name)
+          expect(new_game_page.other_teams.count).to eq 4
+        end
+      end
+
+      context 'creating a new game that i won' do
+        before do
+          new_game_page.my_team_select.select(Player.first.name)
+          new_game_page.other_team_select.select(Team.second.team_name)
+          new_game_page.submit.click
+        end
+
+        scenario 'renders the root path' do
+          expect(page.current_path).to eq root_path
+        end
+
+        scenario 'creates a game on the db' do
+          expect(Game.count).to eq 1
+          expect(Game.first.has_winner?).to eq true
+        end
+
+        scenario 'sets a game winner to my team' do
+          winning_players = Game.first.winning_players
+          expect(winning_players.include?(signed_in_player)).to eq true
+        end
       end
     end
   end
--- spec/page_objects/new_game.rb
+++ spec/page_objects/new_game.rb
@@ -3,10 +3,12 @@ class NewGame < SitePrism::Page
 
   element :header, 'h1#new-team-page-header'
   element :form_container, '#new-game-form'
-  elements :teams, 'div input'
   element :submit_button, 'input#submit-new-game'
 
   # Select boxes
-  elements :teams, 'input[id^="game_team_ids_"]'
+  element :my_team_select, 'select.my-team'
+  element :other_team_select, 'select.other-team'
+  elements :my_teams, '.my-team option'
+  elements :other_teams, 'select.other-team option'
   element :submit, '#submit-new-game'
 end
